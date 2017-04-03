e4b9a263372b48db9bee79ae2db93b119673376f
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-03 20:59:09 +0200

    response status

--- src/main/java/com/github/mkopylec/projectmanager/api/TeamsEndpoint.java
+++ src/main/java/com/github/mkopylec/projectmanager/api/TeamsEndpoint.java
@@ -4,10 +4,14 @@ import com.github.mkopylec.projectmanager.api.dto.NewTeam;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RequestMapping("/teams")
 public interface TeamsEndpoint {
 
+    @ResponseStatus(CREATED)
     @PostMapping
     void createTeam(NewTeam newTeam);
 }
