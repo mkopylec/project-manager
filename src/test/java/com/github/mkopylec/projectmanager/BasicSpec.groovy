49836c6636e12a874d22f3fa55276ae263e18b16
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-03 19:48:53 +0200

    reporting service stub

--- src/test/java/com/github/mkopylec/projectmanager/BasicSpec.groovy
+++ src/test/java/com/github/mkopylec/projectmanager/BasicSpec.groovy
@@ -1,5 +1,7 @@
 package com.github.mkopylec.projectmanager
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import org.junit.Rule
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -8,6 +10,10 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import spock.lang.Specification
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.post
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import static org.springframework.http.HttpMethod.GET
 import static org.springframework.http.HttpMethod.POST
@@ -16,8 +22,14 @@ import static org.springframework.http.HttpMethod.PUT
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 abstract class BasicSpec extends Specification {
 
+    @Rule
+    public WireMockRule reportingService = new WireMockRule(8081)
     @Autowired
     private TestRestTemplate restTemplate
+
+    protected void stubReportingService() {
+        stubFor(post(urlEqualTo('/reports/projects')).willReturn(aResponse().withStatus(201)))
+    }
 
     protected <T> ResponseEntity<T> get(String uri, Class<T> responseBodyType) {
         return sendRequest(uri, GET, null, responseBodyType)
