f49b04f8200cf170f68232e751a24ea776727dda
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-05 19:28:34 +0200

    step 2 tests

--- src/test/groovy/com/github/mkopylec/projectmanager/specification/TeamSpecification.groovy
+++ src/test/groovy/com/github/mkopylec/projectmanager/specification/TeamSpecification.groovy
@@ -64,16 +64,103 @@ class TeamSpecification extends BasicSpecification {
     }
 
     @Unroll
-    def "Should not a new member to a team with empty first name"() {
+    def "Should add a new member with '#jobPosition' job position to a team"() {
         given:
-        def newTeam = new NewTeam(name: 'Team 3')
+        def newTeam = new NewTeam(name: 'Team 4')
         post('/teams', newTeam)
-        def member = new NewTeamMember(firstName: 'Mariusz', lastName: 'Kopylec', jobPosition: 'developer')
+        def member = new NewTeamMember(firstName: 'Mariusz', lastName: 'Kopylec', jobPosition: jobPosition)
 
         when:
-        def response = post('/teams/Team 3/members', member)
+        def response = post('/teams/Team 4/members', member)
 
         then:
         response.statusCode == CREATED
+
+        where:
+        jobPosition << ['developer', 'DEVELOPER', 'product owner', 'Product Owner', 'product_owner', 'PRODUCT_OWNER']
+    }
+
+    @Unroll
+    def "Should not add a new member without a first name to a team"() {
+        given:
+        def newTeam = new NewTeam(name: 'Team 5')
+        post('/teams', newTeam)
+        def member = new NewTeamMember(firstName: firstName, lastName: 'Kopylec', jobPosition: 'developer')
+
+        when:
+        def response = post('/teams/Team 5/members', member)
+
+        then:
+        response.statusCode == UNPROCESSABLE_ENTITY
+        response.body.code == 'EMPTY_MEMBER_FIRST_NAME'
+
+        where:
+        firstName << [null, '', '  ']
+    }
+
+    @Unroll
+    def "Should not add a new member without a last name to a team"() {
+        given:
+        def newTeam = new NewTeam(name: 'Team 6')
+        post('/teams', newTeam)
+        def member = new NewTeamMember(firstName: 'Mariusz', lastName: lastName, jobPosition: 'developer')
+
+        when:
+        def response = post('/teams/Team 6/members', member)
+
+        then:
+        response.statusCode == UNPROCESSABLE_ENTITY
+        response.body.code == 'EMPTY_MEMBER_LAST_NAME'
+
+        where:
+        lastName << [null, '', '  ']
+    }
+
+    @Unroll
+    def "Should not add a new member without job position to a team"() {
+        given:
+        def newTeam = new NewTeam(name: 'Team 7')
+        post('/teams', newTeam)
+        def member = new NewTeamMember(firstName: 'Mariusz', lastName: 'Kopylec', jobPosition: jobPosition)
+
+        when:
+        def response = post('/teams/Team 7/members', member)
+
+        then:
+        response.statusCode == UNPROCESSABLE_ENTITY
+        response.body.code == 'EMPTY_MEMBER_JOB_POSITION'
+
+        where:
+        jobPosition << [null, '', '  ']
+    }
+
+    @Unroll
+    def "Should not add a new member with '#jobPosition' job position to a team"() {
+        given:
+        def newTeam = new NewTeam(name: 'Team 8')
+        post('/teams', newTeam)
+        def member = new NewTeamMember(firstName: 'Mariusz', lastName: 'Kopylec', jobPosition: jobPosition)
+
+        when:
+        def response = post('/teams/Team 8/members', member)
+
+        then:
+        response.statusCode == UNPROCESSABLE_ENTITY
+        response.body.code == 'INVALID_MEMBER_JOB_POSITION'
+
+        where:
+        jobPosition << ['accountant', 'Project Manager', 'TECHNICAL_LEADER']
+    }
+
+    def "Should not add a new member to a nonexistent team"() {
+        given:
+        def member = new NewTeamMember(firstName: 'Mariusz', lastName: 'Kopylec', jobPosition: 'developer')
+
+        when:
+        def response = post('/teams/Team 9/members', member)
+
+        then:
+        response.statusCode == UNPROCESSABLE_ENTITY
+        response.body.code == 'NONEXISTENT_TEAM'
     }
 }
