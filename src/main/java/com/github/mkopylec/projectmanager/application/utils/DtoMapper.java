d298f5977419e23deeddc29f6444206ba0007dae
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-06 20:55:43 +0200

    step 3 finish not test check

---
+++ src/main/java/com/github/mkopylec/projectmanager/application/utils/DtoMapper.java
@@ -0,0 +1,31 @@
+package com.github.mkopylec.projectmanager.application.utils;
+
+import com.github.mkopylec.projectmanager.application.dto.ExistingTeam;
+import com.github.mkopylec.projectmanager.application.dto.TeamMember;
+import com.github.mkopylec.projectmanager.domain.team.Team;
+
+import static java.util.stream.Collectors.toList;
+
+public class DtoMapper {
+
+    public static ExistingTeam mapToExistingTeam(Team team) {
+        ExistingTeam existingTeam = new ExistingTeam();
+        existingTeam.setName(team.getName());
+        existingTeam.setBusy(team.isBusy());
+        existingTeam.setCurrentlyImplementedProjects(team.getCurrentlyImplementedProjects());
+        existingTeam.setMembers(team.getMembers().stream()
+                .map(employee -> {
+                    TeamMember member = new TeamMember();
+                    member.setFirstName(employee.getFirstName());
+                    member.setLastName(employee.getLastName());
+                    member.setJobPosition(employee.getJobPosition().toString());
+                    return member;
+                })
+                .collect(toList())
+        );
+        return existingTeam;
+    }
+
+    private DtoMapper() {
+    }
+}
