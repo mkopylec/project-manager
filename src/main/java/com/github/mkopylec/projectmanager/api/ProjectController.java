375764d8d871688c725e836d033817420aacdeec
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-07 20:46:15 +0200

    step 4 start, no tests

---
+++ src/main/java/com/github/mkopylec/projectmanager/api/ProjectController.java
@@ -0,0 +1,22 @@
+package com.github.mkopylec.projectmanager.api;
+
+import com.github.mkopylec.projectmanager.application.dto.NewProjectDraft;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/projects")
+class ProjectController {
+
+    @ResponseStatus(CREATED)
+    @PostMapping("/drafts")
+    public void createProject(@RequestBody NewProjectDraft newProjectDraft) {
+
+    }
+}
