07727054277a63b0708c65ee5f1ae67f05694a79
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-10 16:52:43 +0200

    format

--- src/main/java/com/github/mkopylec/projectmanager/domain/project/Project.java
+++ src/main/java/com/github/mkopylec/projectmanager/domain/project/Project.java
@@ -18,9 +18,12 @@ public class Project {
     private Status status;
 
     Project(String identifier, String name, Status status) {
-        when(isBlank(identifier)).thenInvalidEntity(EMPTY_PROJECT_IDENTIFIER, "Error creating '" + name + "'project");
-        when(isBlank(name)).thenInvalidEntity(EMPTY_PROJECT_NAME, "Error creating '" + identifier + "'project");
-        when(status == null).thenInvalidEntity(EMPTY_PROJECT_STATUS, "Error creating '" + name + "'project");
+        when(isBlank(identifier))
+                .thenInvalidEntity(EMPTY_PROJECT_IDENTIFIER, "Error creating '" + name + "'project");
+        when(isBlank(name))
+                .thenInvalidEntity(EMPTY_PROJECT_NAME, "Error creating '" + identifier + "'project");
+        when(status == null)
+                .thenInvalidEntity(EMPTY_PROJECT_STATUS, "Error creating '" + name + "'project");
         this.identifier = identifier;
         this.name = name;
         this.status = status;
