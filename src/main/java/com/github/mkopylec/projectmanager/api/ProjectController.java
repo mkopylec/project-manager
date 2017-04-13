095b756f6ae046f76bfdd7805a925a53bef20007
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2017-04-13 12:21:28 +0200

    step 8 start

--- src/main/java/com/github/mkopylec/projectmanager/api/ProjectController.java
+++ src/main/java/com/github/mkopylec/projectmanager/api/ProjectController.java
@@ -7,16 +7,19 @@ import com.github.mkopylec.projectmanager.application.dto.ExistingProject;
 import com.github.mkopylec.projectmanager.application.dto.ExistingProjectDraft;
 import com.github.mkopylec.projectmanager.application.dto.NewProject;
 import com.github.mkopylec.projectmanager.application.dto.NewProjectDraft;
+import com.github.mkopylec.projectmanager.application.dto.UpdatedProject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -51,5 +54,11 @@ class ProjectController {
     @GetMapping("/{projectIdentifier}")
     public ExistingProject getProject(@PathVariable String projectIdentifier) {
         return projectService.getProject(projectIdentifier);
+    }
+
+    @ResponseStatus(NO_CONTENT)
+    @PutMapping("/{projectIdentifier}")
+    public void updateProject(@PathVariable String projectIdentifier, @RequestBody UpdatedProject updatedProject) {
+
     }
 }
