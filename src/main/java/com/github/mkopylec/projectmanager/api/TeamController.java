c838d50a1965abc15d50c8f2731a77e6460d6e9a
Mariusz Kopylec <mariusz.kopylec@allegro.pl>
2018-05-04 09:08:07 +0200

    imports format

--- src/main/java/com/github/mkopylec/projectmanager/api/TeamController.java
+++ src/main/java/com/github/mkopylec/projectmanager/api/TeamController.java
@@ -1,7 +1,6 @@
 package com.github.mkopylec.projectmanager.api;
 
 import com.github.mkopylec.projectmanager.application.dto.NewTeam;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
